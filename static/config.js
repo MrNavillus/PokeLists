f01b7cedde3d97a230d8630b5cc0eba9f9c8a3bc
MrNavillus <31326446+MrNavillus@users.noreply.github.com>
2017-09-10 13:27:25 -0400

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,21 +2,21 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "2PACX-1vRCpLCMGHNacxyDq1GZWMhzjZBwBM7KzSSkDpKjplXivhobLrPE_cvJtAl0NbfnFpvddvSzGNuKwiMz";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "3368-1751-8273";
+var inGameName = "Tsuki";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/Mr_Navillus";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://github.com/MrNavillus/PM_ICONS/blob/master/382baad0-44fd-11e7-be6a-0a1aa813c6b7.png";
